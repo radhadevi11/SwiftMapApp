ab6557e9d7a7e0785616c01f063962f5ed7d0be3
radhadevih <radhadevih@presidio.com>
2024-09-30 11:46:32 +0530

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Radha Devi H on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
