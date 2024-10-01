599168cfa4537bd7ca3ffc8268700e820874e55f
radhadevi11 <radhadevi16isr033@gmail.com>
2024-10-01 09:07:48 +0530

    Complete Map app creation with list view, preview and location detail

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -14,13 +14,84 @@ struct LocationsView: View {
 	
     var body: some View {
 		ZStack {
-			Map(position: $vm.mapPosition)
+			mapLayer
 				.ignoresSafeArea()
+			VStack {
+				header
+					.padding()
+				Spacer()
+				locationsPreviewStack
+
+			}
 		}
+		.sheet(item: $vm.sheetLocation, onDismiss: nil) { location in
+			LocationDetailView(location: location)
+		}
+		
 	
     }
 }
 
+extension LocationsView {
+	private var header: some View {
+		VStack {
+			Button(action: vm.toggleLocationsList) {
+				Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+					.font(.title2)
+					.fontWeight(.black)
+					.foregroundColor(.primary)
+					.frame(height: 55)
+					.frame(maxWidth: .infinity)
+					.animation(.none, value: vm.mapLocation)
+					.overlay(alignment: .leading) {
+						Image(systemName: "arrow.down")
+							.font(.headline)
+							.foregroundColor(.primary)
+							.padding()
+							.rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
+					}
+			}
+			
+			if vm.showLocationsList {
+				LocationsListView()
+			}
+		}
+		.background(.thickMaterial)
+		.cornerRadius(10)
+		.shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+	}
+	
+	private var locationsPreviewStack: some View {
+		ZStack {
+			ForEach(vm.locations) { location in
+				if vm.mapLocation == location {
+					LocationPreviewView(location: location)
+						.shadow(color: Color.black.opacity(0.3), radius: 20)
+						.padding()
+						.frame(maxWidth: .infinity)
+						.transition(.asymmetric(
+							insertion: .move(edge: .trailing),
+							removal: .move(edge: .leading)))
+				}
+			}
+		}
+	}
+	
+	private var mapLayer: some View {
+		Map(position: $vm.mapPosition) {
+			ForEach(vm.locations) { location in
+				Annotation(location.name, coordinate: location.coordinates) {
+					LocationMapAnnotationView()
+						.scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+						.shadow(radius: 10)
+						.onTapGesture {
+							vm.showNextLocation(location: location)
+						}
+				}
+			}
+		}
+	}
+}
 #Preview {
 		LocationsView().environmentObject(LocationsViewModel())
 	}
